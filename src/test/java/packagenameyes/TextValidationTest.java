b15062b22dbd48824e1a14f2af3228b7ab531e22
zhunzargulhane <43793149+zhunzargulhane@users.noreply.github.com>
2021-01-31 16:09:49 +0530

    Update TextValidationTest.java

--- src/test/java/packagenameyes/TextValidationTest.java
+++ src/test/java/packagenameyes/TextValidationTest.java
@@ -34,7 +34,8 @@ public class TextValidationTest extends Baseclass{
 		WebDriverWait wait=new WebDriverWait(driver,30);
 		wait.until(ExpectedConditions.visibilityOf(lp.popup()));
 		lp.popup().click();
-		log.info("pop up window is closed successfully");
+		//log.info("pop up window is closed successfully");
+		System.out.println("pop up window is closed successfully");
 		/*super.browserclose();
 		Thread.sleep(5000);
 		super.initializeDriver();
@@ -43,10 +44,12 @@ public class TextValidationTest extends Baseclass{
 		lp1.popup().click();*/
 		//compare the text from browser with our actual value 
 		// and if anything fails then it should stop the execution
-		log.debug("comparing the title");
+		//log.debug("comparing the title");
+		System.out.println("comparing the title");
 		String gettext=lp.featured().getText();
 		Assert.assertEquals(gettext,"FEATURED COURSES");
-		log.info("done successfully");
+		//log.info("done successfully");
+		System.out.println("done successfully");
 		
 		//boolean boo=lp1.navigation().isDisplayed();
 		//Assert.assertTrue(boo);
