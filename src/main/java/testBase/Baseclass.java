5d4c816d2f83159bc93f3f71993d8a8c9d20e82d
zhunzargulhane <43793149+zhunzargulhane@users.noreply.github.com>
2021-02-07 10:49:10 +0530

    Update Baseclass.java

--- src/main/java/testBase/Baseclass.java
+++ src/main/java/testBase/Baseclass.java
@@ -22,6 +22,7 @@ import org.apache.logging.log4j.LogManager;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.PageLoadStrategy;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
@@ -33,7 +34,6 @@ import org.testng.annotations.AfterMethod;
 import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeSuite;
-
 import com.aventstack.extentreports.ExtentReports;
 import com.aventstack.extentreports.ExtentTest;
 import com.aventstack.extentreports.reporter.ExtentHtmlReporter;
